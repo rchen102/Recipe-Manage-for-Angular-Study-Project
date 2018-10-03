f71408876468f06dc2ab5fe61fa93dc3b35cb301
rchen102 <rchen102@binghamton.edu>
2018-10-02 23:29:36 -0400

    create project

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+
+@Injectable()
+export class RecipeService {
+
+	private recipes: Recipe[] = [
+		new Recipe(
+			'A Test Recipe', 
+			'This is simply test', 
+			'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2003/9/29/0/ig1a07_roasted_potatoes.jpg.rend.hgtvcom.616.462.suffix/1446840363593.jpeg',
+			[
+				new Ingredient('Meat',1),
+				new Ingredient('French Fries', 20)
+			]),
+		new Recipe(
+			'Another Test Recipe', 
+			'This is simply test', 
+			'https://static01.nyt.com/images/2018/06/05/dining/31COOKING-GREEK-SHRIMP1/31COOKING-GREEK-SHRIMP1-articleLarge.jpg',
+			[
+				new Ingredient('Buns', 2),
+				new Ingredient('Bread',1)
+			])
+	];
+
+	constructor(private slService: ShoppingListService) {}
+
+	getRecipes() {
+		return this.recipes.slice();
+	}
+
+	getRecipe(index: number) {
+		return this.recipes[index];
+	}
+
+	addIngredientsToShoppingList(ingredients: Ingredient[]) {
+		this.slService.addIngredients(ingredients);
+	}
+}
