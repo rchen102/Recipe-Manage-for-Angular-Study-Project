a91433ab124dc6bb3334ec4387a5709553c8069b
rchen102 <rchen102@binghamton.edu>
2018-11-01 00:35:18 -0400

    work on route protection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,16 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
+
+@Injectable()
 export class AuthService {
+	token: string;
+
+	constructor(private router: Router) {}
+
 	signupUser(email: string, password: string) {
 		firebase.auth().createUserWithEmailAndPassword(email, password)
 			.catch(
@@ -12,10 +21,34 @@ export class AuthService {
 	signinUser(email: string, password: string) {
 		firebase.auth().signInWithEmailAndPassword(email, password)
 			.then(
-				response => console.log(response)
+				response => {
+					this.router.navigate(['/']);
+					firebase.auth().currentUser.getIdToken()
+						.then(
+							(token: string) => this.token = token
+						);
+				}
 			)
 			.catch(
 				error => console.log(error)
 			);
+	}
+
+	logout() {
+		this.router.navigate(['/signin']);
+		firebase.auth().signOut();
+		this.token = null;
+	}
+
+	getToken() {
+		firebase.auth().currentUser.getIdToken()
+			.then(
+				(token: string) => this.token = token
+			);
+		return this.token;
+	}
+
+	isAuthenticated() {
+		return this.token != null;
 	}
 }
