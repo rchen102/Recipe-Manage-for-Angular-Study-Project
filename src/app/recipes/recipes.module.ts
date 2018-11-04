8572eacbb7ac9ca5c737bfbcd68675bab3ab6c71
rchen102 <rchen102@binghamton.edu>
2018-11-03 22:42:02 -0400

    restructure and lazy loading

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+
+@NgModule({
+	declarations: [
+		RecipesComponent,
+		RecipeStartComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeItemComponent
+	],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		RecipesRoutingModule,
+		SharedModule
+	]
+})
+export class RecipesModule {}
